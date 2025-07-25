080dd3fc7af43f8c281be58cad48866183922fbc
Coslu <bariscoslu@gmail.com>
2025-07-26 01:00:25 +0200

    Remove shadow from JobProperty

--- composeApp/src/commonMain/kotlin/com/coslu/jobtracker/components/JobProperty.kt
+++ composeApp/src/commonMain/kotlin/com/coslu/jobtracker/components/JobProperty.kt
@@ -3,16 +3,15 @@ package com.coslu.jobtracker.components
 import androidx.compose.animation.core.MutableTransitionState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -23,6 +22,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.minimumInteractiveComponentSize
+import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,7 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.isSpecified
 import androidx.compose.ui.input.pointer.PointerIcon
@@ -78,7 +79,7 @@ fun JobProperty(
                 }
             }
         }
-        val propertyColor = remember { getPropertyColor(property) }
+        val propertyColor = getPropertyColor(property)
         PopupBubble(
             dpOffset = DpOffset(25.dp, (-35).dp),
             visible = showFullName,
@@ -112,48 +113,56 @@ fun BigProperty(
     onLongClick: () -> Unit = {}
 ) {
     val propertyColor = getPropertyColor(property)
-    val shadowSize = if (propertyColor != PropertyColor.Transparent) 5.dp else 0.dp
-    Row(modifier = Modifier.padding(5.dp)) {
-        var modifier = Modifier.shadow(shadowSize, RoundedCornerShape(30))
-            .background(propertyColor.color, shape = RoundedCornerShape(30))
-            .wrapContentWidth()
-        if (clickable)
-            modifier = modifier.combinedClickable(onClick = onClick, onLongClick = onLongClick)
+    val interactionSource = remember { MutableInteractionSource() }
+    val shape = RoundedCornerShape(30)
+    val modifier = Modifier.minimumInteractiveComponentSize().padding(5.dp)
+        .background(propertyColor.color, shape = shape).clip(shape)
+    Box(
+        modifier = if (clickable) modifier.combinedClickable(
+            onClick = onClick,
+            onLongClick = onLongClick,
+            interactionSource = interactionSource,
+            indication = ripple()
+        ) else modifier,
+    ) {
+        Text(
+            text = property,
+            modifier = Modifier
+                .padding(start = 10.dp, end = 10.dp, top = 5.dp, bottom = 5.dp),
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            color = if (propertyColor.textColor.isSpecified) propertyColor.textColor else MaterialTheme.colorScheme.onBackground
+        )
+    }
+}
+
+@Composable
+fun SmallProperty(property: String, onClick: () -> Unit = {}, onLongClick: () -> Unit = {}) {
+    if (property.isEmpty())
+        Spacer(Modifier.size(45.dp))
+    else {
+        val propertyColor = getPropertyColor(property)
+        val interactionSource = remember { MutableInteractionSource() }
+        val shape = RoundedCornerShape(50)
         Box(
-            modifier = modifier,
+            modifier = Modifier.minimumInteractiveComponentSize().padding(5.dp).size(40.dp)
+                .background(propertyColor.color, shape = shape).clip(shape)
+                .combinedClickable(
+                    onClick = onClick,
+                    onLongClick = onLongClick,
+                    interactionSource = interactionSource,
+                    indication = ripple()
+                ),
             contentAlignment = Alignment.Center
         ) {
             Text(
-                property,
+                text = property.first().toString(),
                 modifier = Modifier
                     .padding(start = 10.dp, end = 10.dp, top = 5.dp, bottom = 5.dp),
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
                 color = if (propertyColor.textColor.isSpecified) propertyColor.textColor else MaterialTheme.colorScheme.onBackground
             )
-        }
-    }
-}
-
-@Composable
-fun SmallProperty(property: String, onClick: () -> Unit = {}, onLongClick: () -> Unit = {}) {
-    val propertyColor = getPropertyColor(property)
-    val shadowSize = if (propertyColor != PropertyColor.Transparent) 5.dp else 0.dp
-    if (property.isEmpty())
-        Spacer(Modifier.size(45.dp))
-    else {
-        Row(modifier = Modifier.padding(start = 5.dp)) {
-            Box(
-                modifier = Modifier.shadow(shadowSize, RoundedCornerShape(50))
-                    .background(propertyColor.color, RoundedCornerShape(50)).size(40.dp)
-                    .combinedClickable(onClick = onClick, onLongClick = onLongClick),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    property.first().toString(),
-                    color = if (propertyColor.textColor.isSpecified) propertyColor.textColor else MaterialTheme.colorScheme.onBackground
-                )
-            }
         }
     }
 }
