2e85397e92f25b6bb4bc40961df246b982d20f1e
Coslu <bariscoslu@gmail.com>
2025-11-09 14:15:42 +0100

    Language through CompositionLocalProvider

--- composeApp/src/commonMain/kotlin/com/coslu/jobtracker/App.kt
+++ composeApp/src/commonMain/kotlin/com/coslu/jobtracker/App.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,6 +36,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.runtime.snapshots.SnapshotStateMap
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.runtime.toMutableStateMap
 import androidx.compose.ui.Alignment
@@ -73,6 +75,7 @@ import kotlinx.datetime.format.char
 import kotlinx.datetime.toLocalDateTime
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
+import java.util.Locale
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
@@ -87,161 +90,170 @@ private lateinit var propertyColors: SnapshotStateMap<String, PropertyColor>
 @OptIn(ExperimentalTime::class, ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
-    TrajanTheme {
-        jobs = remember {
-            Job.list.sortedWith(sortingMethod.comparator).toMutableStateList()
-        }
-        LaunchedEffect(Unit) { fetchSettings(); Settings.applyFilters() }
-        propertyColors = remember { fetchPropertyColors().toMutableStateMap() }
-        val showJobDialog = remember { MutableTransitionState(false) }
-        val showFilters = remember { MutableTransitionState(false) }
-        val showSettings = remember { MutableTransitionState(false) }
-        var selectedJob by remember { mutableStateOf<Job?>(null) }
-        coroutineScope = rememberCoroutineScope()
-        snackbarHostState = remember { SnackbarHostState() }
-        Scaffold(snackbarHost = { SnackbarHost(snackbarHostState) }) { contentPadding ->
-            AnimatedVisibility(
-                !jobs.any { it.visible.targetState },
-                Modifier.padding(contentPadding),
-                enter = fadeIn(),
-                exit = fadeOut()
-            ) {
+    var currentLanguage by Settings.Language.current
+    CompositionLocalProvider(staticCompositionLocalOf { currentLanguage.locale } provides currentLanguage.locale) {
+        Locale.setDefault(currentLanguage.locale)
+        TrajanTheme {
+            jobs = remember {
+                Job.list.sortedWith(sortingMethod.comparator).toMutableStateList()
+            }
+            LaunchedEffect(Unit) { fetchSettings(); Settings.applyFilters() }
+            propertyColors = remember { fetchPropertyColors().toMutableStateMap() }
+            val showJobDialog = remember { MutableTransitionState(false) }
+            val showFilters = remember { MutableTransitionState(false) }
+            val showSettings = remember { MutableTransitionState(false) }
+            var selectedJob by remember { mutableStateOf<Job?>(null) }
+            coroutineScope = rememberCoroutineScope()
+            snackbarHostState = remember { SnackbarHostState() }
+            Scaffold(snackbarHost = { SnackbarHost(snackbarHostState) }) { contentPadding ->
+                AnimatedVisibility(
+                    !jobs.any { it.visible.targetState },
+                    Modifier.padding(contentPadding),
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    Column(
+                        Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painterResource(Res.drawable.logo),
+                            null,
+                            modifier = Modifier.padding(
+                                start = 10.dp,
+                                top = 10.dp,
+                                end = 10.dp,
+                                bottom = 40.dp
+                            ),
+                            alpha = 0.4f
+                        )
+                    }
+                }
                 Column(
-                    Modifier.fillMaxSize(),
+                    modifier = Modifier.fillMaxSize().padding(contentPadding),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
                 ) {
-                    Image(
-                        painterResource(Res.drawable.logo),
-                        null,
-                        modifier = Modifier.padding(
-                            start = 10.dp,
-                            top = 10.dp,
-                            end = 10.dp,
-                            bottom = 40.dp
-                        ),
-                        alpha = 0.4f
-                    )
-                }
-            }
-            Column(
-                modifier = Modifier.fillMaxSize().padding(contentPadding),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
-            ) {
-                listState = rememberLazyListState()
-                LazyColumn(modifier = Modifier.weight(1f), state = listState) {
-                    item {
-                        Box(modifier = Modifier.height(1.dp))
-                    }
-                    items(jobs) {
-                        val showNotes =
-                            remember { MutableTransitionState(false).apply { targetState = false } }
-                        AnimatedVisibility(
-                            it.visible,
-                            enter = expandIn(),
-                            exit = shrinkOut(),
-                        ) {
-                            Column(Modifier.padding(20.dp)) {
-                                Row(verticalAlignment = Alignment.CenterVertically) {
-                                    JobName(it.name, it.url, Modifier.weight(1f))
-                                    Text(
-                                        Instant.fromEpochMilliseconds(it.date)
-                                            .toLocalDateTime(TimeZone.currentSystemDefault())
-                                            .format(
-                                                LocalDateTime.Format {
-                                                    day()
-                                                    char('.')
-                                                    monthNumber()
-                                                    char('.')
-                                                    year()
-                                                }
-                                            ),
-                                        modifier = Modifier.padding(horizontal = 10.dp)
-                                    )
-                                }
-                                Row(Modifier, verticalAlignment = Alignment.CenterVertically) {
-                                    Row(Modifier.weight(1f).padding(start = 40.dp)) {
-                                        JobProperty(it.type, Modifier.weight(1f, false))
-                                        JobProperty(it.location, Modifier.weight(1f, false))
-                                        JobProperty(it.status, Modifier.weight(1f, false))
-                                        if (it.notes.isNotEmpty()) {
-                                            TooltipButton(
-                                                description = stringResource(Res.string.show_notes),
-                                                onClick = {
-                                                    showNotes.targetState = !showNotes.currentState
-                                                },
-                                            ) {
-                                                PopupBubble(
-                                                    dpOffset = DpOffset(34.dp, 20.dp),
-                                                    visible = showNotes,
-                                                    text = it.notes,
-                                                )
-                                                Icon(
-                                                    painterResource(Res.drawable.notes),
-                                                    stringResource(Res.string.show_notes),
-                                                    tint = MaterialTheme.colorScheme.primary
-                                                )
-                                            }
-                                        }
+                    listState = rememberLazyListState()
+                    LazyColumn(modifier = Modifier.weight(1f), state = listState) {
+                        item {
+                            Box(modifier = Modifier.height(1.dp))
+                        }
+                        items(jobs) {
+                            val showNotes =
+                                remember {
+                                    MutableTransitionState(false).apply {
+                                        targetState = false
                                     }
-                                    TooltipButton(
-                                        description = stringResource(Res.string.edit),
-                                        onClick = {
-                                            selectedJob = it
-                                            showJobDialog.targetState = true
-                                        }
-                                    ) {
-                                        Icon(
-                                            painterResource(Res.drawable.edit),
-                                            stringResource(Res.string.show_notes)
+                                }
+                            AnimatedVisibility(
+                                it.visible,
+                                enter = expandIn(),
+                                exit = shrinkOut(),
+                            ) {
+                                Column(Modifier.padding(20.dp)) {
+                                    Row(verticalAlignment = Alignment.CenterVertically) {
+                                        JobName(it.name, it.url, Modifier.weight(1f))
+                                        Text(
+                                            Instant.fromEpochMilliseconds(it.date)
+                                                .toLocalDateTime(TimeZone.currentSystemDefault())
+                                                .format(
+                                                    LocalDateTime.Format {
+                                                        day()
+                                                        char('.')
+                                                        monthNumber()
+                                                        char('.')
+                                                        year()
+                                                    }
+                                                ),
+                                            modifier = Modifier.padding(horizontal = 10.dp)
                                         )
                                     }
+                                    Row(Modifier, verticalAlignment = Alignment.CenterVertically) {
+                                        Row(Modifier.weight(1f).padding(start = 40.dp)) {
+                                            JobProperty(it.type, Modifier.weight(1f, false))
+                                            JobProperty(it.location, Modifier.weight(1f, false))
+                                            JobProperty(it.status, Modifier.weight(1f, false))
+                                            if (it.notes.isNotEmpty()) {
+                                                TooltipButton(
+                                                    description = stringResource(Res.string.show_notes),
+                                                    onClick = {
+                                                        showNotes.targetState =
+                                                            !showNotes.currentState
+                                                    },
+                                                ) {
+                                                    PopupBubble(
+                                                        dpOffset = DpOffset(34.dp, 20.dp),
+                                                        visible = showNotes,
+                                                        text = it.notes,
+                                                    )
+                                                    Icon(
+                                                        painterResource(Res.drawable.notes),
+                                                        stringResource(Res.string.show_notes),
+                                                        tint = MaterialTheme.colorScheme.primary
+                                                    )
+                                                }
+                                            }
+                                        }
+                                        TooltipButton(
+                                            description = stringResource(Res.string.edit),
+                                            onClick = {
+                                                selectedJob = it
+                                                showJobDialog.targetState = true
+                                            }
+                                        ) {
+                                            Icon(
+                                                painterResource(Res.drawable.edit),
+                                                stringResource(Res.string.show_notes)
+                                            )
+                                        }
+                                    }
                                 }
+                                HorizontalDivider()
                             }
-                            HorizontalDivider()
                         }
                     }
-                }
-                BottomBar(
-                    actions = arrayOf(
-                        BottomBarAction(
-                            stringResource(Res.string.settings),
-                            Res.drawable.settings
-                        ) {
-                            showSettings.targetState = true
-                        },
-                        BottomBarAction(
-                            stringResource(Res.string.sort_filter),
-                            Res.drawable.sort_filter
-                        ) {
-                            showFilters.targetState = true
-                        },
-                        BottomBarAction(stringResource(Res.string.add_job), Res.drawable.add) {
-                            selectedJob = null
-                            showJobDialog.targetState = true
-                        }
+                    BottomBar(
+                        actions = arrayOf(
+                            BottomBarAction(
+                                stringResource(Res.string.settings),
+                                Res.drawable.settings
+                            ) {
+                                showSettings.targetState = true
+                            },
+                            BottomBarAction(
+                                stringResource(Res.string.sort_filter),
+                                Res.drawable.sort_filter
+                            ) {
+                                showFilters.targetState = true
+                            },
+                            BottomBarAction(stringResource(Res.string.add_job), Res.drawable.add) {
+                                selectedJob = null
+                                showJobDialog.targetState = true
+                            }
+                        )
                     )
-                )
-            }
-            val settingsNavController = rememberNavController()
-            SideSheet(
-                showSideSheet = showSettings,
-                modifier = Modifier.padding(contentPadding),
-                navController = settingsNavController
-            ) {
-                SettingsNavHost(settingsNavController)
-            }
-            SideSheet(showFilters, Modifier.padding(contentPadding)) {
-                SortAndFilter()
-            }
-            val showDeleteDialog = remember { MutableTransitionState(false) }
-            SideSheet(showJobDialog, Modifier.padding(contentPadding), true, showDeleteDialog) {
-                JobDialog(
-                    onDismissRequest = { showJobDialog.targetState = false },
-                    selectedJob,
-                    showDeleteDialog
-                )
+                }
+                val settingsNavController = rememberNavController()
+                SideSheet(
+                    showSideSheet = showSettings,
+                    modifier = Modifier.padding(contentPadding),
+                    navController = settingsNavController
+                ) {
+                    SettingsNavHost(settingsNavController)
+                }
+                SideSheet(showFilters, Modifier.padding(contentPadding)) {
+                    SortAndFilter()
+                }
+                val showDeleteDialog = remember { MutableTransitionState(false) }
+                SideSheet(showJobDialog, Modifier.padding(contentPadding), true, showDeleteDialog) {
+                    JobDialog(
+                        onDismissRequest = { showJobDialog.targetState = false },
+                        selectedJob,
+                        showDeleteDialog
+                    )
+                }
             }
         }
     }
